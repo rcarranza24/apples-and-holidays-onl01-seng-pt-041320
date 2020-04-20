1a9cb8cee451f89de6a7eb89f067b127169d2641
Rosa Carranza <rcarranza1024@yahoo.com>
2020-04-20 00:02:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -16,7 +16,8 @@ def second_supply_for_fourth_of_july(holiday_hash)
     :memorial_day => ["BBQ"]
   }
 }
-  # return the second element in the 4th of July array
+  def second_supply_for_fourth_of_july(holiday_supplies)
+  holiday_supplies[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
